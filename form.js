b30a493d415fe047890a4cf708f88885ee4e438f
yacobraju <64398146+yacobraju@users.noreply.github.com>
2020-09-03 20:18:11 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  render() {
+    return <div>position="bottom-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover</div>;
+  }
+}
+
+export default Form;
